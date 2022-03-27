b11347f98cca3d993e27c5d61e7f7347c3d1ae8f
AdeshMask <adeshmaske255@gmail.com>
2022-03-27 17:36:41 +0530

    Added:Max Integer Number

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,8 +1,26 @@
 package com.bridgelabz;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static int findMax(Integer x, Integer y,Integer z){
+        Integer max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        else if (z.compareTo(max)>0)
+            max = z;
+        return max;
     }
+    public static void main(String[] args) {
+        System.out.println("Finding the Maximum number from given numbers:");
+        System.out.println("Enter 3 numbers: ");
+        Integer x = scanner.nextInt();
+        Integer y = scanner.nextInt();
+        Integer z = scanner.nextInt();
+        //findMax(x,y,z);
+        System.out.println("Max: "+findMax(x,y,z));
+    }
+
 }
