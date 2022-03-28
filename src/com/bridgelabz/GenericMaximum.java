e84ccf7a9a1cb6f3e0c0819da31a4469956b2d6a
AdeshMask <adeshmaske255@gmail.com>
2022-03-28 18:42:36 +0530

    Added:printMax method

--- src/com/bridgelabz/GenericMaximum.java
+++ src/com/bridgelabz/GenericMaximum.java
@@ -10,17 +10,19 @@ public class GenericMaximum<T extends Comparable <T>>{
             max = z;
         if (n.compareTo(max) > 0)
             max = n;
+        printMax(x,y,z,n,max);
         return max;
     }
-    
+    public static <T> void printMax(T x,T y,T z,T n,T max){
+        System.out.printf("Max of %s, %s, %s, %s is:: ",x,y,z,n,max);
+    }
     public static void main(String[] args) {
         GenericMaximum max = new GenericMaximum();
-        System.out.println("Maximum of 3 Integer numbers: ");
         Integer aInt = 10,bInt = 32,cInt = 211, dInt = 45;
-        System.out.println("Maximum of 3 Integer array is: "+max.findMax(aInt,bInt,cInt,dInt));
+        System.out.println(max.findMax(aInt,bInt,cInt,dInt));
         Float pFloat = 12.0f,qFloat = 43.7f,rFloat = 54.0f,sFloat = 39.90f;
-        System.out.println("Maximum of 3 Integer array is: "+max.findMax(pFloat,qFloat,rFloat, sFloat));
+        System.out.println(max.findMax(pFloat,qFloat,rFloat, sFloat));
         String str1 = "Apple",str2 =  "Peach",str3 =  "Banana", str4 = "Pineapple";
-        System.out.println("Maximum of 3 Integer array is: "+max.findMax(str1,str2,str3,str4));
+        System.out.println(max.findMax(str1,str2,str3,str4));
     }
 }
